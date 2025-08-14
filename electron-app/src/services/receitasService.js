8448b81c88f8fde30f2ce65b5046efd694299ecc
Rafael <59062891+rafael2297@users.noreply.github.com>
2025-08-14 20:09:19 -0300

    Delete electron-app directory

--- electron-app/src/services/receitasService.js
+++
@@ -1,9 +0,0 @@
-// src/services/receitasService.js
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/receitas";
-
-export const listarReceitas = () => axios.get(API_URL);
-export const adicionarReceita = (receita) => axios.post(API_URL, receita);
-export const atualizarReceita = (id, receita) => axios.put(`${API_URL}/${id}`, receita);
-export const deletarReceita = (id) => axios.delete(`${API_URL}/${id}`);
--- electron-app/src/services/relatoriosService.js
+++
@@ -1,7 +0,0 @@
-// src/services/relatoriosService.js
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/relatorios";
-
-export const gerarRelatorio = () =>
-    axios.get(`${API_URL}/excel`, { responseType: "blob" });
